1ca7d8d165c60caa08396de197927c559b625379
hadley <h.wickham@gmail.com>
2013-05-02 12:02:02 -0400

    Mark objects

--- R/mark.R
+++ R/mark.R
@@ -1,23 +1,41 @@
 
+mark <- function(type, ...) {
+  structure(list(...), class = c(paste0("mark_", type), "mark"))
+}
+print.mark <- function(x, ...) str(x)
+
 mark_point <- function(x = NULL, y = NULL, opacity = NULL,
                        fill = NULL, fillOpacity = NULL,
                        stroke = NULL, strokeWidth = NULL, strokeOpacity = NULL,
                        size = NULL, shape = NULL) {
+
+  mark("point", x = x, y = y, opacity = opacity,
+    fill = fill, fillOpacity = fillOpacity,
+    stroke = stroke, strokeWidth = strokeWidth, strokeOpacity = strokeOpacity,
+    size = size, shape = shape)
 
 }
 
 mark_line <- function(x = NULL, y = NULL, opacity = NULL,
                       stroke = NULL, strokeWidth = NULL, strokeOpacity = NULL) {
 
+  mark("line", x = x, y = y, opacity = opacity,
+    stroke = stroke, strokeWidth = strokeWidth, strokeOpacity = strokeOpacity)
 }
 
 mark_ribbon <- function(x = NULL, y = NULL, y2 = NULL, opacity = NULL,
                         fill = NULL, fillOpacity = NULL,
                         stroke = NULL, strokeWidth = NULL, strokeOpacity = NULL) {
 
+  mark("ribbon", x = x, y = y, y2 = y2, opacity = opacity,
+    fill = fill, fillOpacity = fillOpacity,
+    stroke = stroke, strokeWidth = strokeWidth, strokeOpacity = strokeOpacity)
 }
 
 mark_rect <- function(x = NULL, x2 = NULL, y = NULL, y2 = NULL, opacity = NULL,
                       fill = NULL, fillOpacity = NULL,
                       stroke = NULL, strokeWidth = NULL, strokeOpacity = NULL) {
+  mark("rect", x = x, x2 = x2, y = y, y2 = y2, opacity = opacity,
+    fill = fill, fillOpacity = fillOpacity,
+    stroke = stroke, strokeWidth = strokeWidth, strokeOpacity = strokeOpacity)
 }
