bd21b1a072151abf885d0d680a5e5f6fcda35132
Winston Chang <winston@stdout.org>
2013-05-06 11:57:41 -0500

    Use S3 method to find vega mark type

--- R/vega.R
+++ R/vega.R
@@ -147,8 +147,6 @@ vega_scale <- function(scale, domain, data) {
 # Given a gigvis mark object, output a vega mark object
 vega_mark <- function(mark, mapping, data) {
 
-  if (inherits(mark, "mark_point"))  type = "symbol"
-
   # Generate the fields related to mappings (x, y, etc)
   # This assumes that the scale's name is the same as the 'name' field, which
   # is true now but might not be a good assumption in the long run.
@@ -162,7 +160,7 @@ vega_mark <- function(mark, mapping, data) {
 
   # TODO: Support other properties besides just stroke and fill
   list(
-    type = type,
+    type = vega_mark_type(mark),
     from = list(data = data),
     properties = list(
       update = c(
@@ -175,3 +173,17 @@ vega_mark <- function(mark, mapping, data) {
     )
   )
 }
+
+
+
+# Given a gigvis mark object, return the vega mark type
+vega_mark_type <- function(mark) UseMethod("vega_mark_type")
+
+#' @S3method vega_mark_type default
+vega_mark_type.default <- function(mark) NULL
+
+#' @S3method vega_mark_type mark_point
+vega_mark_type.mark_point <- function(mark) "symbol"
+
+#' @S3method vega_mark_type mark_line
+vega_mark_type.mark_line <- function(mark) "line"
