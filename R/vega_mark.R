7ef8d17b2c57d8d26683ce2a8afd7e6d692acf33
Winston Chang <winston@stdout.org>
2013-05-08 14:35:43 -0500

    Better method for getting mark properties

--- R/vega_mark.R
+++ R/vega_mark.R
@@ -41,13 +41,51 @@ vega_mark_type.mark_line <- function(mark) "line"
 
 
 # Given a gigvis mark object, return a list of vega mark properties
-vega_mark_properties <- function(mark) UseMethod("vega_mark_properties")
-
-#' @S3method vega_mark_properties default
-vega_mark_properties.default <- function(mark) {
-  # For most marks, we can remove some gigvis-specific fields, then remove
-  # the class, drop nulls, and convert to proper format for vega properties
-  mark[c("type", "data", "data_std", "mapping")] <- NULL
+vega_mark_properties <- function(mark) {
+  # Keep only the vega-specific fields, then remove the class, drop nulls,
+  # and convert to proper format for vega properties.
+  mark <- mark[names(mark) %in% valid_vega_mark_properties(mark)]
   mark <- unclass(drop_nulls(mark))
   lapply(mark, function(x) list(value=x))
 }
+
+
+# Given a gigvis mark object, return a vector of strings of valid vega
+# mark properties
+valid_vega_mark_properties <- function(mark) {
+  UseMethod("valid_vega_mark_properties")
+}
+
+#' @S3method valid_vega_mark_properties default
+valid_vega_mark_properties.default <- function(mark) {
+  .common_vega_mark_properties
+}
+
+#' @S3method valid_vega_mark_properties symbol
+valid_vega_mark_properties.symbol <- function(mark) {
+  c(.common_vega_mark_properties, "size", "shape")
+}
+
+#' @S3method valid_vega_mark_properties area
+valid_vega_mark_properties.area <- function(mark) {
+  c(.common_vega_mark_properties, "interpolate", "tension")
+}
+
+#' @S3method valid_vega_mark_properties line
+valid_vega_mark_properties.line <- function(mark) {
+  c(.common_vega_mark_properties, "interpolate", "tension")
+}
+
+#' @S3method valid_vega_mark_properties image
+valid_vega_mark_properties.image <- function(mark) {
+  c(.common_vega_mark_properties, "url", "align", "baseline")
+}
+
+#' @S3method valid_vega_mark_properties text
+valid_vega_mark_properties.text <- function(mark) {
+  c(.common_vega_mark_properties, "text", "align", "baseline", "dx", "dy",
+    "angle", "font", "fontSize", "fontWeight", "fontStyle")
+}
+
+.common_vega_mark_properties <- c("x", "x2", "width", "y", "y2", "height",
+  "opacity", "fill", "fillOpacity", "stroke", "strokeWidth", "strokeOpacity")
