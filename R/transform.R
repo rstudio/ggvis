58486bb26f531ef36ee2e587140c0737bb50cf01
Winston Chang <winston@stdout.org>
2013-05-21 18:32:09 -0500

    Calculate predictions with original variables

--- R/transform.R
+++ R/transform.R
@@ -35,64 +35,67 @@ compute <- function(transform, data, mapping) UseMethod("compute")
 
 #' @S3method compute transform_smooth
 compute.transform_smooth <- function(transform, data, mapping) {
+  xvar <- mapping["x"]
+  yvar <- mapping["y"]
+
   if (transform$method == "auto") {
     transform$method <- if (nrow(data) > 1000) "gam" else "loess"
   }
   if (transform$formula == "auto") {
-    transform$formula <- if(transform$method == "gam") y ~ s(x) else y ~ x
+    if (transform$method == "gam")
+      transform$formula <- as.formula(paste(yvar, "~ s(", xvar, ")"))
+    else
+      transform$formula <- as.formula(paste(yvar, "~", xvar))
   }
   transform$method <- as.name(transform$method)
-
-
-  # Rename the columns specified in mapping. For example, if mapping is
-  # c(x="wt", y="mpg"), then this renames data's 'wt' column to 'x', and
-  # 'mpg' to 'y'.
-  names(data)[match(mapping, names(data))] <- names(mapping)
 
   call <- substitute(method(transform$formula, data = data), transform["method"])
   call <- modify_call(call, transform$dots)
   mod <- eval(call)
 
-  xseq <- seq(min(data$x), max(data$x), length = transform$n)
+  xseq <- seq(min(data[[xvar]]), max(data[[xvar]]), length = transform$n)
 
   # Make prediction
-  pred_data <- predictdf(mod, xseq, transform$se, transform$level)
-
-  # Rename the columns back to original names
-  names(pred_data)[match(names(mapping), names(pred_data))] <- mapping
+  pred_data <- predictdf(mod, xseq, xvar, yvar, transform$se, transform$level)
 
   pred_data
 }
 
 
 
-predictdf <- function(model, xseq, se, level) UseMethod("predictdf")
+predictdf <- function(model, xseq, xvar, yvar, se, level) UseMethod("predictdf")
 
 #' @S3method predictdf default
-predictdf.default <- function(model, xseq, se, level) {
-  pred <- stats::predict(model, newdata = data.frame(x = xseq), se = se,
+predictdf.default <- function(model, xseq, xvar, yvar, se, level) {
+  dat <- data.frame(v1 = xseq)
+  names(dat) <- xvar
+  pred <- stats::predict(model, newdata = dat, se = se,
     level = level, interval = if(se) "confidence" else "none")
 
   if (se) {
     fit <- as.data.frame(pred$fit)
-    names(fit) <- c("y", "ymin", "ymax")
-    data.frame(x = xseq, fit, se = pred$se)
+    names(fit) <- c(yvar, paste0("_", yvar, "_min"), paste0("_", yvar, "_max"))
+    dat <- cbind(dat, fit, se = pred$se)
   } else {
-    data.frame(x = xseq, y = as.vector(pred))
+    dat[[yvar]] <- as.vector(pred)
   }
+  dat
 }
 
 #' @S3method predictdf loess
-predictdf.loess <- function(model, xseq, se, level) {
-  pred <- stats::predict(model, newdata = data.frame(x = xseq), se = se)
+predictdf.loess <- function(model, xseq, xvar, yvar, se, level) {
+  dat <- data.frame(v1 = xseq)
+  names(dat) <- xvar
+  pred <- stats::predict(model, newdata = dat, se = se)
 
   if (se) {
-    y = pred$fit
+    dat[[yvar]] <- pred$fit
     ci <- pred$se.fit * qt(level / 2 + .5, pred$df)
-    ymin = y - ci
-    ymax = y + ci
-    data.frame(x = xseq, y, ymin, ymax, se = pred$se.fit)
+    dat[[paste0("_", yvar, "_min")]] <- y - ci
+    dat[[paste0("_", yvar, "_max")]] <- y + ci
+    dat$se <- pred$se.fit
   } else {
-    data.frame(x = xseq, y = as.vector(pred))
+    dat[[yvar]] <- as.vector(pred)
   }
+  dat
 }
