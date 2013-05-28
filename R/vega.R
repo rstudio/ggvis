0f59805e917f7497517d84b709eafc24ef0a378f
Winston Chang <winston@stdout.org>
2013-05-28 15:06:48 -0500

    Export vega_spec

--- R/vega.R
+++ R/vega.R
@@ -3,6 +3,7 @@
 #'
 #' @param envir The environment in which to evaluate the \code{data} parameter
 #'   of the gigvis object.
+#' @export
 vega_spec <- function(gv,
                       width = 600, height = 400, padding = c(20, 20, 30, 50),
                       envir = parent.frame()) {
