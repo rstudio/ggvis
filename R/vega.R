d9e9f04f432b6725086e18198e7fe70c20a2e0b0
Winston Chang <winston@stdout.org>
2013-06-03 17:38:36 -0500

    Faster implementation of d3df.data.frame

--- R/vega.R
+++ R/vega.R
@@ -198,13 +198,26 @@ vega_df <- function(x, name) {
 }
 
 
-
+# Convert a data object to a D3-structured data object.
+# Numbers and strings stay the same type; everything else gets converted to
+# strings with as.character().
 d3df <- function(x) UseMethod("d3df")
 
 #' @S3method d3df data.frame
 d3df.data.frame <- function(x) {
-  n <- nrow(x)
-  lapply(seq_len(n), function(i) as.list(x[i, ]))
+  rows <- nrow(x)
+  colnames <- setNames(names(x), names(x))
+
+  x <- lapply(x, function(col) {
+    if (is.numeric(col) || is.character(col))  col
+    else  as.character(col)
+  })
+
+  lapply(seq_len(rows), function(i) {
+    lapply(colnames, function(colname) {
+      .subset2(.subset2(x, colname), i)
+    })
+  })
 }
 
 #' @S3method d3df split_data_dflist
