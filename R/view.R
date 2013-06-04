c251d97a3192d3a4a7c582f92bab96549f83cb75
Winston Chang <winston@stdout.org>
2013-06-04 17:13:33 -0500

    Add functions for generating static images.

--- R/view.R
+++ R/view.R
@@ -36,6 +36,75 @@ view_static <- function(gv, envir = parent.frame(), launch = TRUE) {
 }
 
 
+#' Generate an PNG file from a gigvis object
+#'
+#' This requires that the program \code{vg2png} is installed. This is part of
+#' \code{vega} node.js module.
+#'
+#' @seealso \url{https://github.com/trifacta/vega} for information on installing
+#'   \code{vg2png}.
+#'
+#' @param gv A gigvis object.
+#' @param envir The environment in which to evaluate \code{gv}.
+#' @param file Output file name. If NULL, defaults to "plot.png".
+#' @export
+vega_png <- function(gv, envir = parent.frame(), file = NULL) {
+  vega_file(gv, envir, file = file, type = "png")
+}
+
+#' Generate a SVG file from a gigvis object
+#'
+#' This requires that the program \code{vg2png} is installed. This is part of
+#' \code{vega} node.js module.
+#'
+#' @seealso \url{https://github.com/trifacta/vega} for information on installing
+#'   \code{vg2svg}.
+#'
+#' @param gv A gigvis object.
+#' @param envir The environment in which to evaluate \code{gv}.
+#' @param file Output file name. If NULL, defaults to "plot.svg".
+#' @export
+vega_svg <- function(gv, envir = parent.frame(), file = NULL) {
+  vega_file(gv, envir, file = file, type = "svg")
+}
+
+# Generate an output image file from a gigvis object
+#
+# @param gv A gigvis object.
+# @param envir The environment in which to evaluate \code{gv}.
+# @param file Output file name. If NULL, defaults to "plot.png".
+# @param type Output file type.
+vega_file <- function(gv, envir = parent.frame(), file = NULL,
+                      type = "png") {
+
+  if (!(type %in% c("png", "svg")))  stop("type must be 'png' or 'svg'")
+
+  if (is.null(file))  file <- paste0("plot.", type)
+
+  temp_dir <- tempfile(pattern = "gigvis")
+  dir.create(temp_dir)
+
+  # Try to find the external program that generates the images
+  cmd <- paste0("vg2", type)
+
+  # Search in these paths and use the first one found
+  cmdsearch <- Sys.which(paste0(c("", "./bin/", "./node_modules/.bin/"), cmd))
+  found_idx <- which(nzchar(cmdsearch))
+  if (length(found_idx) == 0)
+    stop("Conversion program ", cmd, "not found.")
+  cmd <- cmdsearch[min(found_idx)]
+
+  # Generate the Vega JSON spec
+  vega_json <- toJSON(vega_spec(gv, envir = envir), pretty = TRUE)
+  json_file <- file.path(temp_dir, "plot.json")
+  cat(vega_json, file = json_file)
+  on.exit(unlink(json_file))
+
+  # Create the image file
+  system2(cmd, args = c(json_file, file))
+}
+
+
 copy_www_resources <- function(destdir) {
   files <- c(
     "lib/jquery-1.9.1.js",
