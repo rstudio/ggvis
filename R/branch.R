25f4592b1a5b6ec591794ee04ea20650b79a9ff2
hadley <h.wickham@gmail.com>
2013-06-19 16:49:47 -0500

    Remove too-early branch function

--- R/branch.R
+++
@@ -1,9 +0,0 @@
-
-# Need to figure out how to choose between transform and mark args
-branch_smooth <- function(..., se = TRUE) {
-  node(
-    transform = transform_smooth(..., se = se),
-    if (se) mark_area(),
-    mark_line()
-  )
-}
