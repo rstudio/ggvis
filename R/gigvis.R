9ed67113b04abdbbab1cf9078a9795e5c0fd0913
Winston Chang <winston@stdout.org>
2013-06-27 17:00:16 -0500

    Don't automatically add scales in gigvis() step

--- R/gigvis.R
+++ R/gigvis.R
@@ -2,8 +2,7 @@
 #' @import assertthat
 gigvis <- function(data = NULL, props = NULL, ..., dynamic = FALSE) {
   structure(
-    node(data = data, props = props, fill_defaults = TRUE, ...,
-      dynamic = dynamic),
+    node(data = data, props = props, ..., dynamic = dynamic),
     class = c("gigvis", "gigvis_node")
   )
 }
@@ -11,40 +10,18 @@ gigvis <- function(data = NULL, props = NULL, ..., dynamic = FALSE) {
 
 #' @export
 node <- function(..., data = NULL, props = NULL, scales = NULL,
-                 fill_defaults = FALSE, dynamic = NULL) {
-  # data is a data object, or a pipeline
-  # props is a named character vector, permissible names are properties
-  #   that vega understands
-  # scales is a list of scale objects
-
+                 dynamic = FALSE) {
   # assert_that(is.character(props), !is.null(names(props)))
-
-  if (fill_defaults) {
-
-    if (is.null(scales))  scales <- list()
-
-    # For each props, add a scale if necessary
-    for (name in names(props)) {
-      if (is.null(scales[[name]])) {
-        scales[[name]] <- scale(name)
-      }
-    }
-  }
-
-  children <- list(...)
-
-  node <- list(
-    data = as.pipeline(data),
-    props = props,
-    scales = scales,
-    children = children
-  )
-
-  if (!is.null(dynamic))
-    node$dynamic <- dynamic
+  if (is.null(scales))  scales <- list()
 
   structure(
-    node,
+    list(
+      data = as.pipeline(data),
+      props = props,
+      scales = scales,
+      children = list(...),
+      dynamic = dynamic
+    ),
     class = "gigvis_node"
   )
 }
@@ -58,6 +35,4 @@ print.gigvis <- function(gv, envir = parent.frame()) {
     view_dynamic(prerendered)
   else
     view_static(prerendered)
-
-
 }
