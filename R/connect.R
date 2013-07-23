17973dc58c793a9b9503c6ce431dc18c28f0e454
hadley <h.wickham@gmail.com>
2013-07-23 13:57:23 -0500

    Add example of reactive pipeline with reactive data (Doesn't currently work)

--- R/connect.R
+++ R/connect.R
@@ -35,6 +35,18 @@
 #' isolate(r())
 #' v$add <- 2
 #' isolate(r())
+#' 
+#' # Reactive pipeline with reactive data
+#' v <- reactiveValues(add = 0, mult = 1, n = 5)
+#' p <- pipeline(
+#'    reactive(df[1:v$n, , ]), 
+#'    transform_scale(add = reactive(v$add), mult = reactive(v$mult))
+#' )
+#' r <- connect(p, asis)
+#' isolate(r())
+#' v$add <- 2
+#' v$n <- 10
+#' isolate(r())
 connect <- function(x, props, data = NULL) {
   stopifnot(is.gigvis_props(props))
   needs_shiny()
