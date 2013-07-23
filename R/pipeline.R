e153f6af87011703cccbda5a9ef666e2a73a1268
hadley <h.wickham@gmail.com>
2013-07-23 11:36:23 -0500

    Move flow to own file

--- R/pipeline.R
+++ R/pipeline.R
@@ -112,39 +112,6 @@ trim_to_source <- function(x) {
   x
 }
 
-#' Flow data down a pipe or pipeline
-#'
-#' This flows data down a pipeline (starting with \code{NULL}), applying
-#' each pipe (transformation) in sequence.
-#'
-#' Every element in a pipeline recieves the same set of properties: this
-#' generally means that for more complicated transformations you will need
-#' to create multiple branches.
-#'
-#' @param x a pipeline or pipe
-#' @param properties a \code{props} object
-#' @param data the data source to start the flow
-#' @export
-#' @keywords internal
-flow <- function(x, props, data = NULL) {
-  stopifnot(is.gigvis_props(props))
-  UseMethod("flow")
-}
-
-#' @S3method flow pipeline
-flow.pipeline <- function(x, props, data = NULL) {
-  for (pipe in x) {
-    data <- flow(pipe, props, data)
-  }
-  data
-}
-
-#' @S3method flow NULL
-flow.NULL <- function(x, data, props) {
-  data
-}
-
-
 #' @S3method split_vars pipeline
 split_vars.pipeline <- function(x) {
   unlist(lapply(x, split_vars), recursive = FALSE)
