be8e341d955f5754394c393bc819862c352f7752
Winston Chang <winston@stdout.org>
2013-07-26 09:28:54 -0500

    Update dynamic examples

--- demo/dynamic.r
+++ demo/dynamic.r
@@ -2,41 +2,47 @@ library(gigvis)
 library(shiny)
 
 # Basic dynamic example
-mtc1 <- reactive({invalidateLater(2000); mtcars[sample(1:10, 5), ]})
-p <- gigvis(
-  data = mtc1,
-  props = props(x ~ wt, y ~ mpg),
-  mark_symbol()
+mtc1 <- reactive({
+  invalidateLater(2000, NULL);
+  mtcars[sample(nrow(mtcars), 10), ]
+})
+gigvis(mtc1, props(x ~ wt, y ~ mpg),
+  mark_symbol(),
+  dynamic = TRUE
 )
-view_dynamic(p)
-
 
 
 # Two separate data sets, equal in the tree
-mtc1 <- reactive({invalidateLater(2000); mtcars[sample(1:10, 5), ]})
-mtc2 <- reactive({invalidateLater(2000); mtcars[sample(11:20, 5), ]})
-# mtc1 <- mtcars[1:10, ]
-# mtc2 <- mtcars[11:20, ]
-p <- gigvis(data = NULL, mapping = aes(x = "wt", y = "mpg"),
-            node(
-              data = mtc1,
-              mark_symbol(stroke = "black", fill = "black")
-            ),
-            node(
-              data = mtc2,
-              mark_symbol(fill = "red", size = 40)
-            )
+mtc1 <- reactive({
+  invalidateLater(2000, NULL);
+  mtcars[sample(nrow(mtcars), 10), ]
+})
+mtc2 <- reactive({
+  invalidateLater(2000, NULL);
+  mtcars[sample(nrow(mtcars), 10), ]
+})
+gigvis(data = NULL, props = props(x ~ wt, y ~ mpg),
+  node(
+    data = mtc1,
+    mark_symbol(stroke = "black", fill = "black")
+  ),
+  node(
+    data = mtc2,
+    mark_symbol(fill = "red", size = 40)
+  ),
+  dynamic = TRUE
 )
-view_dynamic(p)
-
 
 # With a transform
-mtc1 <- reactive({invalidateLater(2000); mtcars[sample(1:nrow(mtcars), 20), ]})
-p <- gigvis(data = mtc1, mapping = aes(x = "wt", y = "mpg"),
-            mark_symbol(stroke = "black", fill = "black"),
-            node(
-              transform = transform_smooth(method = "lm", se = F),
-              mark_line(stroke = "red")
-            )
+mtc1 <- reactive({
+  invalidateLater(2000, NULL);
+  mtcars[sample(nrow(mtcars), 10), ]
+})
+gigvis(mtc1, props(x ~ wt, y ~ mpg),
+  mark_symbol(),
+  node(
+    data = transform_smooth(method = "lm"),
+    mark_line(stroke = "red")
+  ),
+  dynamic = TRUE
 )
-view_dynamic(p)
