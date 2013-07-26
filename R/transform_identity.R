a031b2dbe14be2bb3368edc62df1e8117a849159
hadley <h.wickham@gmail.com>
2013-07-26 17:47:19 -0500

    Identity transformation

---
+++ R/transform_identity.R
@@ -0,0 +1,21 @@
+#' Transformation: identity.
+#'
+#' This transformation simply evaluates each property in the context of the
+#' data.
+#' @export
+#' @examples
+#' transform_identity()
+#' sluice(transform_identity(), props(x ~ disp), mtcars)
+transform_identity <- function() {
+  transform("identity")
+}
+
+#' @S3method format transform_identity
+format.transform_identity <- function(x, ...) {
+  paste0(" -> identity")
+}
+
+#' @S3method compute transform_identity
+compute.transform_identity <- function(x, props, data) {
+  data.frame(lapply(props, prop_value, data))
+}
