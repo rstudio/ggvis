d580a95dc72833a9f7e8d08e8f3e22b0bdec4e22
hadley <h.wickham@gmail.com>
2013-07-30 15:43:30 -0500

    Better gigvis & save_spec docs. Also add is.gigvis

--- R/gigvis.R
+++ R/gigvis.R
@@ -1,3 +1,14 @@
+#' Create a gigvis object, or a node in a gigvis plot.
+#'
+#' @param data A data \code{\link{pipeline}}, or anything that can be coerced
+#'   to a pipeline using \code{\link{as.pipeline}}
+#' @param props a list of \code{\link{props}} defining default mark properties
+#'   for this node and all its children.
+#' @param ... children \code{node}s or \code{\link{marks}}.
+#' @param dynamic if \code{TRUE}, this printing this plot will generate a 
+#'   dynamic shiny app; if \code{FALSE}, creates a static html file. See
+#'   also \code{\link{save_spec}} to just output the vega spec
+#' @param scales a \code{\link{scales}} object, to override the default scales
 #' @export
 #' @import assertthat
 gigvis <- function(data = NULL, props = NULL, ..., dynamic = FALSE, 
@@ -16,8 +27,12 @@ gigvis <- function(data = NULL, props = NULL, ..., dynamic = FALSE,
   )
 }
 
+#' @export
+#' @rdname gigvis
+is.gigvis <- function(x) inherits(x, "gigvis")
 
 #' @export
+#' @rdname gigvis
 node <- function(..., data = NULL, props = NULL) {
   structure(
     list(
@@ -44,12 +59,15 @@ print.gigvis <- function(x, ...) {
 #' 
 #' These functions are mainly useful for testing.
 #' 
+#' @param x a gigvis object
+#' @param path location to save spec to, or load spec from
+#' @param ... other arguments passed to \code{as.vega}
 #' @keywords internal
 #' @export
 save_spec <- function(x, path, ...) {
-  gr <- gigvis_render(x, ...)
+  assert_that(is.gigvis(x), is.string(path))
   
-  json <- toJSON(gr$spec, pretty = TRUE)
+  json <- toJSON(as.vega(x, ...), pretty = TRUE)
   writeLines(json, path)
 }
 
