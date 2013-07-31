c9bfb4c2eeac38061c0ca85aa2af2d95ecd7f211
Winston Chang <winston@stdout.org>
2013-07-31 11:43:26 -0500

    Fix dynamic examples for new props interface

--- demo/dynamic.r
+++ demo/dynamic.r
@@ -40,11 +40,11 @@ mtc2 <- reactive({
 gigvis(data = NULL, props = props(x ~ wt, y ~ mpg),
   node(
     data = mtc1,
-    mark_symbol(stroke = "black", fill = "black")
+    mark_symbol(props(stroke = "black", fill = "black"))
   ),
   node(
     data = mtc2,
-    mark_symbol(fill = "red", size = 40)
+    mark_symbol(props(fill = "red", size = 40))
   ),
   dynamic = TRUE
 )
@@ -58,7 +58,7 @@ gigvis(mtc1, props(x ~ wt, y ~ mpg),
   mark_symbol(),
   node(
     data = transform_smooth(method = "lm"),
-    mark_line(stroke = "red")
+    mark_line(props(stroke = "red"))
   ),
   dynamic = TRUE
 )
