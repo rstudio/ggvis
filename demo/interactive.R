b839049feeda80bfbde526c4b3c0ce98ee9f74b0
hadley <h.wickham@gmail.com>
2013-08-02 12:34:35 -0500

    Tweak interactive demo

--- demo/interactive.R
+++ demo/interactive.R
@@ -5,8 +5,7 @@ library(shiny)
 gigvis(mtcars, props(x ~ wt, y ~ mpg),
   mark_symbol(),
   branch_smooth(
-    n = input_slider(label = "Number of interpolation points",
-                     value = 5, min = 2, max = 80, step = 1),
-    method = input_select(choices = c("Linear" = "lm", "LOESS" = "loess"))
+    n = input_slider(2, 80, "Interpolation points", value = 5, step = 1),
+    method = input_select(c("Linear" = "lm", "LOESS" = "loess"))
   )
 )
