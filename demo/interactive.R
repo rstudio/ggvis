1f1f008cbccf37698fffcedf2747f775338bbf56
Winston Chang <winston@stdout.org>
2013-08-06 23:11:02 -0500

    Fix labels

--- demo/interactive.R
+++ demo/interactive.R
@@ -4,8 +4,8 @@ library(gigvis)
 gigvis(mtcars, props(x ~ wt, y ~ mpg),
   mark_symbol(),
   branch_smooth(
-    n = input_slider(2, 80, "Interpolation points", value = 5, step = 1),
-    method = input_select(c("Linear" = "lm", "LOESS" = "loess"))
+    n = input_slider(2, 80, value = 5, step = 1, label = "Interpolation points"),
+    method = input_select(c("Linear" = "lm", "LOESS" = "loess"), label = "Method")
   )
 )
 
