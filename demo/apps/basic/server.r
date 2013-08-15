116163006d459dd852aea40a384b688ddede45c7
hadley <h.wickham@gmail.com>
2013-08-15 12:41:08 -0500

    Tweaks to basic demo

--- demo/apps/basic/server.r
+++ demo/apps/basic/server.r
@@ -1,37 +1,28 @@
 shinyServer(function(input, output, session) {
-
   # Create a slider that's used for two different plots
-  span_slider <- input_slider(min = 0.2, max = 1, value = 0.5, step = 0.1,
-    label = "LOESS span")
+  span_slider <- input_slider(min = 0.2, max = 1, value = 0.5, step = 0.05,
+    label = "Loess span")
 
-
-  # Create the gigvis object
+  # Create the gigvis objects
   gv <- reactive({
     gigvis(mtcars, props(x ~ wt, y ~ mpg),
       mark_symbol(),
       branch_smooth(span = span_slider)
     )
   })
-
-  # Set up observers for the spec and the data
-  observeGigvis(gv, "plot1", session)
-
-  # User interface elements (in the sidebar)
-  output$gigvis_ui <- renderControls(gv, session)
-
-
-  # A second plot
   gv2 <- reactive({
-    gigvis(mtcars,
-      props(x ~ wt, y ~ hp),
+    gigvis(mtcars, props(x ~ wt, y ~ hp),
       mark_symbol(),
       branch_smooth(span = span_slider)
     )
   })
+  
+  # Set up observers for the spec and the data
+  observeGigvis(gv, "plot1", session)
   observeGigvis(gv2, "plot2", session, width = 250, height = 250)
-
-  # Second plot shares controls with first, so no need to renderControls for it
-
+  
+  # User interface elements (in the sidebar)
+  output$gigvis_ui <- renderControls(gv, session)
 
   # Stop the app when the quit button is clicked
   observe({
