ebcc47fc3940fd274c9a64f12c6ab9001610461e
hadley <h.wickham@gmail.com>
2013-08-15 15:39:49 -0500

    Don't document unexported prop generics

--- R/prop.R
+++ R/prop.R
@@ -10,49 +10,34 @@ prop <- function(subclass, ...) {
 #' @rdname prop
 is.prop <- function(x) inherits(x, "prop")
 
-#' Given a property and a dataset, get the value of the property.
-#'
-#' @param x a vega property
-#' @param data a object containing data
-#' @param processed Has this data object been processed so that new columns
-#'   have been calculated and unused columns have been dropped?
+# Given a property and a dataset, get the value of the property.
 prop_value <- function(x, data, processed) {
   UseMethod("prop_value")
 }
 
-#' The name of the property.
-#'
-#' Used for naming the variable it produces in the vega data frame
-#'
-#' @param x a vega property
+# The name of the property: used for naming the variable it produces in the 
+# vega data frame
 prop_name <- function(x) {
   UseMethod("prop_name")
 }
 
-#' The scale (if any) that this property needs
-#'
+# The scale (if any) that this property needs
 prop_scale <- function(x, default_scale) {
   UseMethod("prop_scale")
 }
 
-#' Generate a vega object for the individual mark.
-#'
-#' Will be inserted in to the vega properties object.
-#' @param x a vega property
-#' @param default_scale default scale to use if not already specified in vega
-#'   object
-#' @keywords internal
+# Generate a vega object for the individual mark.
 prop_vega <- function(x, default_scale) {
   UseMethod("prop_vega")
 }
 
-#' Determine the variable type given a data frame and property.
-#'
-#' @param data The data object.
-#' @param prop The property object.
-#' @param processed Has this data object been processed so that new columns
-#'   have been calculated and unused columns have been dropped?
-#' @keywords internal
+# Determine the variable type given a data frame and property.
+#
+# @param data The data object.
+# @param prop The property object.
+# @param processed Has this data object been processed so that new columns
+#   have been calculated and unused columns have been dropped?
+# @keywords internal
 prop_type <- function(data, prop, processed = FALSE) {
   UseMethod("prop_type")
 }
