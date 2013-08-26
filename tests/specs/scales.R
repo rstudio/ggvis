0871140fd49138d762216e61a4a0726e300c3139
hadley <h.wickham@gmail.com>
2013-08-26 11:03:30 -0500

    Some basic spec tests for scales

---
+++ tests/specs/scales.R
@@ -0,0 +1,29 @@
+# Modifying scales
+library(ggvis)
+
+set.seed(1014)
+df <- data.frame(x = 1:5, y = 5:1, z = runif(5))
+
+# Override default scale property
+ggvis(df, props(x ~ x, y ~ x),
+  mark_symbol(),
+  dscale("x", "numeric", trans = "log"),
+  dscale("y", "numeric", trans = "log")
+)
+save_spec("scales/log.json")
+
+# Override default scale name
+ggvis(df, props(x ~ x, y ~ x, fill = prop(quote(z), scale = "blah")),
+  mark_symbol(),
+  dscale("fill", "numeric", name = "blah")
+)
+save_spec("scales/custom.json")
+
+# Dual scale
+ggvis(df, props(x ~ x),
+  mark_symbol(props(y = prop(quote(y), scale = "y-y"), fill = "red")),
+  mark_symbol(props(y = prop(quote(z), scale = "y-z"))),
+  dscale("y", "numeric", name = "y-y"),
+  dscale("y", "numeric", name = "y-z")
+)
+save_spec("scales/dual.json")
