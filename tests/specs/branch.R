9f575e9d24f94aa405e518f52a7e395a24de4f1f
hadley <h.wickham@gmail.com>
2013-08-27 14:45:56 -0500

    Branch regression tests

---
+++ tests/specs/branch.R
@@ -0,0 +1,22 @@
+# Modifying scales
+library(ggvis)
+
+ggvis(mtcars, props(x ~ wt),
+  branch_histogram(binwidth = 1)
+)
+save_spec("branch/histogram.json")
+
+ggvis(mtcars, props(x ~ wt, stroke ~ cyl), by_group(cyl),
+  branch_freqpoly(binwidth = 1)
+)
+save_spec("branch/freqpoly-grouped.json")
+
+ggvis(mtcars, props(x ~ wt, y ~ mpg),
+  branch_smooth()
+)
+save_spec("branch/smooth.json")
+
+ggvis(mtcars, props(x ~ wt, y ~ mpg), by_group(cyl),
+  branch_smooth(method = "lm")
+)
+save_spec("branch/smooth-grouped.json")
