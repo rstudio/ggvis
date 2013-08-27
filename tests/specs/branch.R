36397284043c6e23c571ad09aa1bbc022bc0b9b2
hadley <h.wickham@gmail.com>
2013-08-27 14:50:13 -0500

    Make branch specs less chatty

--- tests/specs/branch.R
+++ tests/specs/branch.R
@@ -12,11 +12,11 @@ ggvis(mtcars, props(x ~ wt, stroke ~ cyl), by_group(cyl),
 save_spec("branch/freqpoly-grouped.json")
 
 ggvis(mtcars, props(x ~ wt, y ~ mpg),
-  branch_smooth()
+  branch_smooth(method = "loess", formula = y ~ x)
 )
 save_spec("branch/smooth.json")
 
 ggvis(mtcars, props(x ~ wt, y ~ mpg), by_group(cyl),
-  branch_smooth(method = "lm")
+  branch_smooth(method = "lm", formula = y ~ x)
 )
 save_spec("branch/smooth-grouped.json")
