1549357e4e7412b0b830d50c8738f49e2cd1e711
Winston Chang <winston@stdout.org>
2013-09-03 15:32:35 -0500

    Update line examples to use transform_sort()

--- demo/lines.r
+++ demo/lines.r
@@ -1,6 +1,12 @@
 library(ggvis)
 
+set.seed(1780)
 df <- data.frame(x = runif(12), y = runif(12), z = gl(3, 4))
 
-ggvis(df, props(x = ~ x, y = ~ y), mark_line())
-ggvis(pipeline(df, by_group(z)), props(x = ~ x, y = ~ y), mark_line())
+ggvis(df, props(x = ~x, y = ~y), mark_line())
+ggvis(pipeline(df, by_group(z)), props(x = ~x, y = ~y, stroke = ~z), mark_line(), mark_symbol())
+
+# Data sorted by x
+ggvis(df, transform_sort(), props(x = ~x, y = ~y), mark_line(), mark_symbol())
+# Data sorted by y
+ggvis(df, transform_sort(var = "y"), props(x = ~x, y = ~y), mark_line(), mark_symbol())
