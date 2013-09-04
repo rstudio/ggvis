946af7da9d417ac3a90bb8017857d3d970028043
Winston Chang <winston@stdout.org>
2013-09-04 12:57:25 -0500

    Update line demos with auto_split

--- demo/lines.r
+++ demo/lines.r
@@ -4,9 +4,22 @@ set.seed(1780)
 df <- data.frame(x = runif(12), y = runif(12), z = gl(3, 4))
 
 ggvis(df, props(x = ~x, y = ~y), mark_line())
-ggvis(pipeline(df, by_group(z)), props(x = ~x, y = ~y, stroke = ~z), mark_line(), mark_symbol())
+
+# Grouping, manually specified
+ggvis(df, by_group(z),
+  props(x = ~x, y = ~y, stroke = ~z),
+  mark_line(),
+  mark_symbol(props(fill := NA))
+)
 
 # Data sorted by x
 ggvis(df, transform_sort(), props(x = ~x, y = ~y), mark_line(), mark_symbol())
 # Data sorted by y
 ggvis(df, transform_sort(var = "y"), props(x = ~x, y = ~y), mark_line(), mark_symbol())
+
+# Grouping with auto_split, and sorted
+ggvis(df, auto_split(), transform_sort(),
+  props(x = ~x, y = ~y, stroke = ~z),
+  mark_line(),
+  mark_symbol(props(fill := NA))
+)
