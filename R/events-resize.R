f170a8bf5d8ac0cf0a5fce2887160ef3bbc22b64
Winston Chang <winston@stdout.org>
2014-01-17 14:43:47 -0600

    Better resize documentation

--- R/events-resize.R
+++ R/events-resize.R
@@ -2,27 +2,38 @@
 NULL
 
 #' Event broker for resize events.
-#' 
+#'
 #' The resize event broker is useful if you want your shiny app to respond
 #' to resize events.
-#' 
+#'
 #' @export
 #' @importFrom methods setRefClass
 Resize <- setRefClass("Resize", contains = "EventBroker",
   methods = list(
     resize = function() {
       "A reactive value changed when the plot is resized."
-      
+
       listen_for("resize")
     }
   )
 )
 
+#' An interactive input bound to resize events.
+#'
+#' @param f A function which is called each time the plot area is resized.
+#'
 #' @export
 #' @examples
+#' \dontrun{}
+#' # This example just prints out the current dimensions to the console
+#' print_info <- function(x) {
+#'   cat(str(x))
+#' }
+#'
 #' ggvis(mtcars, props(x = ~mpg, y = ~wt)) +
 #'   mark_symbol() +
-#'   resize()
+#'   resize(print_info)
+#' }
 resize <- function(f) {
   stopifnot(is.function(f))
   handler("resize", "resize", list(f = f))
