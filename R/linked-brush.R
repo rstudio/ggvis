d7ee9912e6413032507befe55d9a92155068e0ea
hadley <h.wickham@gmail.com>
2014-01-23 14:51:24 -0600

    More progress on lb

--- R/linked-brush.R
+++ R/linked-brush.R
@@ -1,9 +1,9 @@
 #' @examples
 #' lb <- linked_brush(keys = 1:nrow(mtcars))
+#' qvis(mtcars, ~disp, ~mpg, fill := lb$fill_prop(), size.brush := 400) + lb$brush_handler()
 #'
-#' qvis(mtcars, ~disp, ~mpg, fill := lb$fill_prop()) + lb$brush_handler()
-#'
-#' ggvis(mtcars, props(x = ~disp, y = ~mpg, fill := )) + mark_point() +
+#' ggvis(mtcars, props(x = ~disp, y = ~mpg, fill := lb$fill_prop())) +
+#'   mark_point() +
 #'   lb$brush_handler()
 
 linked_brush <- function(keys, fill = "red") {
@@ -12,17 +12,17 @@ linked_brush <- function(keys, fill = "red") {
 
 #' @importFrom shiny reactiveValues
 LinkedBrush <- setRefClass("LinkedBrush",
-  fields = c("keys", "fill", "rv", "brush"),
+  fields = c("keys", "fill", "rv", "brush", "id"),
   methods = list(
     initialize = function(keys = NULL, fill = "red", ...) {
       stopifnot(is.character(fill), length(fill) == 1)
 
       initFields(keys = keys, fill = fill, ...)
 
+      id <<- rand_id()
+
       rv <<- reactiveValues()
       rv$keys <<- character()
-
-      brush <<- Brush()
     },
 
     selected_prop = function() {
@@ -39,13 +39,7 @@ LinkedBrush <- setRefClass("LinkedBrush",
     },
 
     brush_handler = function() {
-      handler("linked_brush", "brush") #, id = brush$id)
-    },
-
-    as_vega = function(session) {
-      session$observe({
-        rv$keys <<- session$input[[message_name(name)]]
-      })
+      handler("linked_brush", "brush", id = rand_id())
     }
   )
 )
@@ -60,8 +54,12 @@ reactive_proxy <- function(rv, name, default, trans = identity) {
 as.reactive.reactive_proxy <- function(x, session = NULL, ...) {
   if (is.null(session)) return(reactive(x$default))
 
-  if (is.null(x$brush$session))
-    x$brush$session <- session
+  brush <- Brush(session, id = x$id)
+  observe({
+    moved <- brush$brush_move()
+    print(moved)
+    x$rv$keys <- moved$keys
+  })
 
   reactive(x$trans(x$rv[[x$name]]))
 }
@@ -75,3 +73,25 @@ print.reactive_proxy <- function(x, ...) {
 controls.reactive_proxy <- function(x, session = NULL, ...) {
   NULL
 }
+
+#' @export
+extract_layer.linked_brush <- function(x, ...) {
+  comps <- parse_components(..., drop_named = TRUE)
+
+  props <- merge_props(
+    props(x := ~x, y := ~y, width := ~width, height := ~height,
+      fill := "black", fillOpacity := 0.2,
+      stroke := "black", strokeOpacity := 0.6,
+      inherit = FALSE),
+    comps$props
+  )
+
+  mark_rect(
+    props,
+    data = pipeline(
+      data.frame(x = 0, y = 0, width = 0, height = 0),
+      .id = "ggvis_brush"
+    )
+  )
+}
+
