208fe70de967a9b6a5db3a20bb4cee2add047655
JJ Allaire <jj@rstudio.org>
2014-02-11 15:23:35 -0500

    add rmarkdown format for ioslides_presentation

--- R/rmarkdown_formats.R
+++ R/rmarkdown_formats.R
@@ -1,6 +1,7 @@
-# Custom format for rmarkdown that includes our special sauce in the head
-# (can be used as a substitute for html_document until we have the right
-# stuff in place in knitr to do this without a special format)
+# Custom html_document and ioslides_presenation formats for rmarkdown
+# that includes our special sauce in the head (can be used as a substitute
+# for the the formats built into rmarkdown until we have the right stuff
+# in place in knitr to do this without a special format)
 #
 # Use within YAML front-matter as follows:
 #
@@ -8,17 +9,33 @@
 # output: ggvis:::html_document
 # ---
 #
-# You can also include any parameters you'd typically specify
-# for html_document:
+# You can also include any parameters you'd typically specify for the
+# base format within the rmarkdown package:
 #
 # ---
 # output:
-#   ggvis:::html_document:
-#     theme: united
-#     highlight: textmate
+#   ggvis:::ioslides_presentation
+#     widescreen: true
 # ---
 #
+
 html_document <- function(...) {
+  rmarkdown::html_document(
+    ...,
+    self_contained = TRUE,
+    includes = rmarkdown_includes()
+  )
+}
+
+ioslides_presentation <- function(...) {
+  rmarkdown::ioslides_presentation(
+    ...,
+    self_contained = TRUE,
+    includes = rmarkdown_includes()
+  )
+}
+
+rmarkdown_includes <- function() {
 
   # Replace relative path with absolute path
   url_abs_path <- function(path) {
@@ -42,14 +59,8 @@ html_document <- function(...) {
     tag
   })
 
-
   ggvis_head_file <- tempfile("ggvis_head", fileext = ".html")
   cat(format(head), file = ggvis_head_file)
 
-  # delegate to rmarkdown html_document
-  rmarkdown::html_document(
-    ...,
-    self_contained = TRUE,
-    includes = rmarkdown::includes(in_header = ggvis_head_file),
-  )
+  rmarkdown::includes(in_header = ggvis_head_file)
 }
