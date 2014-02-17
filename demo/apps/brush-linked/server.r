f910a062cadf755c5fd9854fb2bbe284ca403882
hadley <h.wickham@gmail.com>
2014-02-17 14:17:24 -0600

    Use new linked brush component for linked brush. Closes #111

--- demo/apps/brush-linked/server.r
+++ demo/apps/brush-linked/server.r
@@ -2,47 +2,27 @@ library(ggvis)
 
 data(diamonds, package = "ggplot2")
 diamonds <- diamonds[sample(1:nrow(diamonds), 1000), ]
-diamonds$id <- sample(1:1000)
 
 shinyServer(function(input, output, session) {
 
+  lb <- linked_brush(keys = 1:nrow(diamonds))
+
   gv1 <- reactive({
     ggvis(diamonds, props(x = ~carat, y = ~price)) +
-      mark_symbol(props(fill.brush := "red", fillOpacity := 0.8, key := ~id)) +
-      layer_brush() +
-      opts(width = 300, height = 300, brush_delay = 100)
-  })
-
-  # Set up observers for the spec and the data
-  observe_ggvis(gv1, "plot1", session)
-
-  # Return the colors for the brushed items
-  selected <- reactive({
-    colors <- rep("black", nrow(diamonds))
-
-    if (is.null(input$ggvis_plot1_brush)) return(colors)
-
-    items <- input$ggvis_plot1_brush$items
-
-    keys <- vapply(items, `[[`, "key__", FUN.VALUE = character(1))
-    keys <- as.integer(keys)
-
-    colors[diamonds$id %in% keys] <- "red"
-    colors
+      mark_symbol(props(fill := lb$fill_prop(), fillOpacity := 0.8,
+        fill.brush := "red")) +
+      lb$brush_handler() +
+      opts(width = 300, height = 300)
   })
 
   gv2 <- reactive({
     ggvis(diamonds, props(x = ~table, y = ~depth)) +
-      mark_symbol(props(fill := ~selected(), fillOpacity := 0.8)) +
+      mark_symbol(props(fill := lb$fill_prop(), fillOpacity := 0.8)) +
       opts(width = 300, height = 300)
   })
 
   # Set up observers for the spec and the data
+  observe_ggvis(gv1, "plot1", session)
   observe_ggvis(gv2, "plot2", session)
-
-  # Print the object that was sent over
-  output$brush_data <- renderPrint({
-     str(input$ggvis_plot1_brush)
-  })
 
 })
--- demo/apps/brush-linked/ui.r
+++ demo/apps/brush-linked/ui.r
@@ -1,6 +1,4 @@
 shinyUI(bootstrapPage(
   ggvis_output("plot1"),
-  ggvis_output("plot2"),
-  h3("Brush data (sent from client to server)"),
-  verbatimTextOutput("brush_data")
+  ggvis_output("plot2")
 ))
