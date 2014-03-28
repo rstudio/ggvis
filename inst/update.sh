e0f4b338a88efc6533f0cc672f69070d05e9c6eb
Winston Chang <winston@stdout.org>
2014-03-28 14:35:30 -0500

    Remove unused Quadtree.js

--- inst/update.sh
+++ inst/update.sh
@@ -8,5 +8,4 @@ curl -O https://raw.github.com/mbostock/d3/master/d3.js
 curl -O https://raw.github.com/mbostock/d3/master/d3.min.js
 curl -O http://code.jquery.com/jquery-1.11.0.min.js
 curl -O http://code.jquery.com/jquery-1.11.0.js
-curl -O https://raw.github.com/mikechambers/ExamplesByMesh/master/JavaScript/QuadTree/src/QuadTree.js
 
