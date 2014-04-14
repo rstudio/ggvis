d2a7774a8d423d9f57f9248c15e01d46b3fb4215
Winston Chang <winston@stdout.org>
2014-04-14 11:27:08 -0500

    Add branch_f function

--- R/branch.R
+++ R/branch.R
@@ -1,12 +1,20 @@
-#' Create a branch
+#' Create a branch, given an expression
 #'
 #' @export
 branch <- function(vis, expr, env = parent.frame()) {
+  fun <- defer(substitute(expr), env, quoted = TRUE)
+  vis %>% branch_f(fun)
+}
+
+
+#' Create a branch, given a function
+#'
+#' @export
+branch_f <- function(vis, fun) {
   # Save current data and props
   old_data  <- vis$cur_data
   old_props <- vis$cur_props
 
-  fun <- defer(substitute(expr), env, quoted = TRUE)
   vis <- fun(vis)
 
   # Restore previous data and props
