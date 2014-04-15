aa75e42ace1e0a90bd4004c235bc11211e81cda7
Winston Chang <winston@stdout.org>
2014-04-15 17:02:24 -0500

    Update demo examples

--- demo/lines.r
+++ demo/lines.r
@@ -3,46 +3,46 @@ library(ggvis)
 set.seed(1780)
 df <- data.frame(x = runif(12), y = runif(12), z = gl(3, 4))
 
-df %>% ggvis(props(x = ~x, y = ~y)) %>% mark_path()
+df %>% ggvis(x = ~x, y = ~y) %>% mark_path()
 
 # Grouping, manually specified
 df %>% group_by(z) %>%
-  ggvis(props(x = ~x, y = ~y, stroke = ~z, fill := NA)) %>%
+  ggvis(x = ~x, y = ~y, stroke = ~z, fill := NA) %>%
   mark_path() %>%
   layer_point()
 
 # Grouping can happen after ggvis() call
 df %>%
-  ggvis(props(x = ~x, y = ~y, stroke = ~z, fill := NA)) %>%
+  ggvis(x = ~x, y = ~y, stroke = ~z, fill := NA) %>%
   group_by(z) %>%
   mark_path() %>%
   layer_point()
 
 # Data sorted by x
-df %>% ggvis(props(x = ~x, y = ~y)) %>%
+df %>% ggvis(x = ~x, y = ~y) %>%
   transform_sort() %>%
   mark_path() %>%
   layer_point()
 
 # layer_line sorts and adds a mark_path
-df %>% ggvis(props(x = ~x, y = ~y)) %>%
+df %>% ggvis(x = ~x, y = ~y) %>%
   layer_line() %>%
   layer_point()
 
 # Data sorted by y
-df %>% ggvis(props(x = ~x, y = ~y)) %>%
+df %>% ggvis(x = ~x, y = ~y) %>%
   transform_sort(vars = "y") %>%
   mark_path() %>%
   layer_point()
 
 # Grouping with auto_group, and sorted
-df %>% ggvis(props(x = ~x, y = ~y, stroke = ~z, fill := NA)) %>%
+df %>% ggvis(x = ~x, y = ~y, stroke = ~z, fill := NA) %>%
   auto_group() %>%
   transform_sort() %>%
   mark_path() %>%
   layer_point()
 
 # Using layer_line, which sorts the data
-df %>% ggvis(props(x = ~x, y = ~y, stroke = ~z, fill := NA)) %>%
+df %>% ggvis(x = ~x, y = ~y, stroke = ~z, fill := NA) %>%
   layer_line() %>%
   layer_point()
--- demo/scatterplot.r
+++ demo/scatterplot.r
@@ -1,7 +1,7 @@
 library(ggvis)
 
 # Basic scatter plot
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>% layer_point()
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>% layer_point()
 
 # Variable transformations
 mtcars %>% ggvis(props(x = ~wt, y = ~wt/mpg)) %>% layer_point()
@@ -9,44 +9,44 @@ mtcars %>% ggvis(props(x = ~factor(cyl), y = ~mpg)) %>% layer_point()
 
 # With colour
 # continuous:
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg, fill = ~cyl)) %>% layer_point()
+mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~cyl) %>% layer_point()
 # and discrete:
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg, fill = ~factor(cyl))) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~factor(cyl)) %>%
   layer_point()
 
 # Use unscaled constant: 10 refers to 10 pixels from top
-mtcars %>% ggvis(props(x = ~wt)) %>%
+mtcars %>% ggvis(x = ~wt) %>%
   layer_point(props(y = ~mpg)) %>%
   layer_point(props(y := 10, fill := "red"))
 
 # Use scaled constant: 10 refers to data space
-mtcars %>% ggvis(props(x = ~wt)) %>%
+mtcars %>% ggvis(x = ~wt) %>%
   layer_point(props(y = ~mpg)) %>%
   layer_point(props(y = 10, fill := "red"))
 
 # Line and point graph
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   layer_line() %>%
   layer_point(props(fill := "red"))
 
 # Two marks
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   layer_point() %>%
   layer_point(props(fill := "red", size := 25))
 
 # Multiple nested layers
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   branch(layer_point()) %>%
   branch(branch(layer_point(props(fill := "red", size := 25))))
 
 # Two marks at different levels of the tree, with different mappings for a
 # variable
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   layer_point() %>%
   layer_point(props(fill := "red", y = ~qsec, size := 25))
 
 # Two y scales
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   layer_point() %>%
   layer_point(props(fill := "red", y = prop(~ qsec, scale = "yq"))) %>%
   dscale("y", "numeric", name = "yq")
@@ -54,26 +54,26 @@ mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
 # Two separate data sets, equal in the tree
 mtc1 <- mtcars[1:10, ]
 mtc2 <- mtcars[11:20, ]
-ggvis(NULL, props(x = ~wt, y = ~mpg)) %>%
+ggvis(NULL, x = ~wt, y = ~mpg) %>%
   layer_point(props(stroke := "black", fill := "black"), data = mtc1) %>%
   layer_point(props(fill := "red", size := 40), mtc2)
 
 # Scatter plot with one set of points with `cyl` mapped to stroke,
 # and another set with `am` mapped to fill
-mtcars %>% ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% ggvis(x = ~wt, y = ~mpg) %>%
   layer_point(props(stroke = ~factor(cyl), fill := NA)) %>%
   layer_point(props(fill = ~factor(am), size := 25))
 
 # Same as previous, but also with (useless) grouping in the layers
-mtcars %>% group_by(factor(cyl)) %>%
-  ggvis(props(x = ~wt, y = ~mpg)) %>%
+mtcars %>% group_by(cyl) %>%
+  ggvis(x = ~wt, y = ~mpg) %>%
   layer_point(props(stroke = ~factor(cyl), fill := NA)) %>%
   layer_point(props(fill = ~factor(am), size := 25))
 
 # Use expression in a prop
-pressure %>% ggvis(props(x = ~temperature, y = ~log(pressure))) %>% layer_point()
+pressure %>% ggvis(x = ~temperature, y = ~log(pressure)) %>% layer_point()
 
 # Use variable from the calling environment
 y_min <- min(log(pressure$pressure))
-pressure %>% ggvis(props(x = ~temperature, y = ~log(pressure) - y_min)) %>%
+pressure %>% ggvis(x = ~temperature, y = ~log(pressure) - y_min) %>%
   layer_point()
