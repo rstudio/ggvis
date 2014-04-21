87b465d79a3e462bac3ce19e8b6c6a740c284ce7
Winston Chang <winston@stdout.org>
2014-04-21 11:16:21 -0500

    Add back reactive_label functions

--- R/utils_reactive.R
+++ R/utils_reactive.R
@@ -6,3 +6,12 @@ as.reactive.function <- function(x, session = NULL, ...) x
 as.reactive.reactive <- function(x, session = NULL, ...) x
 #' @export
 as.reactive.default <- function(x, session = NULL, ...) reactive(x, ...)
+
+reactive_label <- function(x) {
+  attr(x, "observable")$.label
+}
+
+`reactive_label<-` <- function(x, value) {
+  attr(x, "observable")$.label <- value
+  x
+}
