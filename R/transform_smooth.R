a1eefc95b4106e444bc3a843ad2839a2875eb90f
hadley <h.wickham@gmail.com>
2014-04-25 08:49:09 -0500

    Polishing smooth/layer_smooth

--- R/transform_smooth.R
+++ R/transform_smooth.R
@@ -1,45 +1,47 @@
 #' Overlay a smooth curve
 #'
 #' \code{layer_smooth} combines \code{\link{smooth}} with \code{mark_path} and
-#' \code{mark_area} to display a smooth line and its standard error.
+#' \code{mark_area} to display 1d model fit to the data in order to smooth it.
+#' It optionally displays the standard error.
 #'
 #' @export
 #' @examples
-#' # Or do it directly with layer_smooth()
 #' mtcars %>% ggvis(~wt, ~mpg) %>% layer_smooth()
-#' mtcars %>% ggvis(~wt, ~mpg) %>% layer_point() %>%
-#'   layer_smooth(method = "lm", se = FALSE)
+#' mtcars %>% ggvis(~wt, ~mpg) %>% layer_smooth(se = T)
 #'
+#' # Control the wiggliness of the loess smoother with the span parameter
+#' mtcars %>% ggvis(~wt, ~mpg) %>% layer_point() %>%
+#'   layer_smooth(span = 0.2)
+#' # Or map to a control and modify interactively
 #' mtcars %>% ggvis(~wt, ~mpg) %>% layer_point() %>%
 #'   layer_smooth(span = input_slider(0.2, 1))
 #'
-#' # These are equivalent to combining transform_smooth with mark_path and
-#' # mark_area
+#' # Use other modelling functions
+#' mtcars %>% ggvis(~wt, ~mpg) %>%
+#'   layer_point() %>%
+#'   layer_smooth(method = "lm") %>%
+#'   layer_smooth(method = "MASS::rlm", props(stroke := "red"))
+#'
+#' # layer_smooth() is just smooth() + mark_path()
+#' mtcars %>% ggvis(~wt, ~mpg) %>% layer_smooth()
 #' mtcars %>%
 #'   ggvis(~wt, ~mpg) %>%
 #'   layer_point() %>%
-#'   transform_smooth() %>%
-#'   mark_area(props(x = ~x, y = ~y_lower__, y2 = ~y_upper__,
-#'    fillOpacity := 0.2)) %>%
-#'   mark_path(props(x = ~x, y = ~y))
+#'   smooth(mpg ~ wt) %>%
+#'   mark_path(props(~pred_, ~resp_, strokeWidth := 2))
 #'
-#' # You can also combine other data transformations like splitting
-#' mtcars %>% ggvis(~wt, ~mpg, stroke = ~cyl) %>% group_by(cyl) %>%
-#'   layer_smooth(method = "lm")
-layer_smooth <- function(vis, props = NULL, formula = NULL, ..., se = FALSE) {
+#' # Run smooth outside of a visualisation to see what variables you get
+#' mtcars %>% smooth(mpg ~ wt)
+layer_smooth <- function(vis, props = NULL, ..., formula = NULL, method = NULL,
+                         se = FALSE) {
 
-  # If formula not supplied, guess it from the x and y props.
-  if (is.null(formula)) {
-    cur_props <- vis$cur_props
-    formula <- eval(substitute(x ~ y,
-      list(x = cur_props$x.update$value, y = cur_props$y.update$value)),
-      parent.frame())
-    notify_guess(formula)
-  }
+  method <- method %||% guess_method(isolate(vis$cur_data()))
+  formula <- formula %||% guess_formula(vis, vis$cur_props, method)
 
-  # Line shouldn't get fill-related props, and se area shouldn't get
-  # stroke-related props.
-  line_props <-  merge_props(props(x = ~pred_, y = ~resp_, strokeWidth := 2), props)
+  # Construct default properties. Line shouldn't get fill-related props,
+  # and se shouldn't get stroke-related props.
+  line_props <- merge_props(props(x = ~pred_, y = ~resp_, strokeWidth := 2),
+    props)
   line_props <- drop_props(line_props, c("fill", "fillOpacity"))
 
   se_props <- merge_props(props(x = ~pred_, y = ~resp_lwr_, y2 = ~resp_upr_,
@@ -47,7 +49,7 @@ layer_smooth <- function(vis, props = NULL, formula = NULL, ..., se = FALSE) {
   se_props <- drop_props(se_props, c("stroke", "strokeOpacity"))
 
   pipeline <- function(x) {
-    x <- smooth(x, formula, ..., se = se)
+    x <- smooth(x, formula = formula, method = method, se = se, ...)
     if (identical(se, TRUE)) {
       x <- mark_area(x, se_props)
     }
@@ -57,65 +59,78 @@ layer_smooth <- function(vis, props = NULL, formula = NULL, ..., se = FALSE) {
   branch_f(vis, pipeline)
 }
 
+guess_formula <- function(vis, props, method) {
+  # FIXME: what's the right way to do this in general?
+  vars <- list(
+    x = props$x.update$value,
+    y = props$y.update$value
+  )
+
+  if (identical(method, "gam")) {
+    f <- substitute(y ~ s(x), vars)
+  } else {
+    f <- substitute(y ~ x, vars)
+  }
+  formula <- eval(f, parent.frame())
+  notify_guess(formula)
+  formula
+}
+
 #' Smooth data with a model.
 #'
-#' Fits a 1d model computes predictions and (optionaly) standard errors.
-#'
-#' @section Ouput:
-#'
-#' \code{smooth} creates a data frame with columns:
-#'
-#' \itemize{
-#'  \item \code{resp_}: regularly spaced grid of \code{n} locations
-#'  \item \code{pred_}: predicted value from smooth
-#' }
-#'
-#' If standard errors are requested, it will also contain:
-#'
-#' \itemize{
-#'  \item \code{pred_lwr_} and \code{pred_upr_}: upper and lower bounds of
-#'    confidence interval
-#'  \item \code{pred_se_}: the standard error (width of the confidence interval)
-#' }
+#' Fit a 1d model, then compute predictions and (optionally) standard errors
+#' over an evenly spaced grid.
 #'
 #' @param method Model fitting function to use - it must support R's standard
 #'   modelling interface, taking a formula and data frame as input, and
 #'   returning predictions with \code{\link{predict}}. If not supplied, will
 #'   use \code{\link{loess}} for <= 1000 points, otherwise it will use
 #'   \code{\link[mgcv]{gam}}. Other modelling functions that will work include
-#'   \code{\link{lm}}, \code{\link{glm}} and \code{\link[MASS]{rlm}}
-#' @param formula Formula passed to modelling function. Can only any variables
+#'   \code{\link{lm}}, \code{\link{glm}} and \code{\link[MASS]{rlm}}.
+#' @param formula Formula passed to modelling function. Can use any variables
 #'   from data.
 #' @param se include standard errors in output? Requires appropriate method of
 #'   \code{predict_grid}, since the interface for returning predictions with
 #'   standard errors is not consistent acrossing modelling frameworks.
 #' @param level the confidence level of the standard errors.
 #' @param n the number of grid points to use in the prediction
-#' @param na.rm If \code{TRUE} missing values will be silently removed,
-#'   otherwise they will be removed with a warning.
-#' @param ... named arguments passed on to \code{method} function, unnamed
-#'   arguments ignored.
+#' @param ... arguments passed on to \code{method} function
+#' @return A data frame with columns:
+#'  \item \code{resp_}: regularly spaced grid of \code{n} locations
+#'  \item \code{pred_}: predicted value from smooth
+#'  \item \code{pred_lwr_} and \code{pred_upr_}: upper and lower bounds of
+#'    confidence interval (if \code{se = TRUE})
+#'  \item \code{pred_se_}: the standard error (width of the confidence interval)
+#'    (if \code{se = TRUE})
 #' @export
 #' @examples
-#' # Use smooth directly on data
 #' mtcars %>% smooth(mpg ~ wt, n = 10)
+#' mtcars %>% smooth(mpg ~ wt, n = 10, se = TRUE)
 #' mtcars %>% group_by(cyl) %>% smooth(mpg ~ wt, n = 10)
+#'
+#' # Override method to suppress message or change approach
+#' mtcars %>% smooth(mpg ~ wt, n = 10, method = "loess")
 #' mtcars %>% smooth(mpg ~ wt, n = 10, method = "lm")
 #'
 #' # Plot the results
 #' mtcars %>% smooth(mpg ~ wt) %>% ggvis(~pred_, ~resp_) %>% layer_line()
 #' mtcars %>% ggvis() %>% smooth(mpg ~ wt) %>% layer_line(props(~pred_, ~resp_))
-smooth <- function(x, formula, ..., method = NULL, se = TRUE,
+smooth <- function(x, formula, ..., method = NULL, se = FALSE,
                    level = 0.95, n = 80L) {
   UseMethod("smooth")
 }
 
 #' @export
-smooth.data.frame <- function(x, formula, ..., method = NULL, se = TRUE,
+smooth.data.frame <- function(x, formula, ..., method = NULL, se = FALSE,
                               level = 0.95, n = 80L) {
   assert_that(is.formula(formula))
   method <- method %||% guess_method(x)
   assert_that(is.string(method))
+
+  if (is.character(method)) {
+    # This allows the use of e.g. MASS::rlm
+    method <- parse(text = method)[[1]]
+  }
 
   # Create model environment & model call, then evaluate
   env <- new.env(parent = environment(formula))
@@ -128,14 +143,14 @@ smooth.data.frame <- function(x, formula, ..., method = NULL, se = TRUE,
 }
 
 #' @export
-smooth.grouped_df <- function(x, formula, ..., method = NULL, se = TRUE,
+smooth.grouped_df <- function(x, formula, ..., method = NULL, se = FALSE,
                               level = 0.95, n = 80L) {
   dplyr::do(x, smooth(., formula = formula, method = method, se = se,
     level = level, n = n, ...))
 }
 
 #' @export
-smooth.ggvis <- function(x, formula, ..., method = NULL, se = TRUE,
+smooth.ggvis <- function(x, formula, ..., method = NULL, se = FALSE,
                          level = 0.95, n = 80L) {
   args <- list(formula = formula, method = method, se = se, level = level,
     n = n, ...)
@@ -162,11 +177,9 @@ notify_guess <- function(x) {
   message("Guessing ", deparse(substitute(x)), " = ", format(x))
 }
 
-
-
 # Helper function to create data frame of predictions -------------------------
 
-pred_grid <- function(model, data, n = 80, se = TRUE, level = 0.95) {
+pred_grid <- function(model, data, n = 80, se = FALSE, level = 0.95) {
   assert_that(is.flag(se))
   assert_that(is.numeric(level), length(level) == 1, level >= 0, level <= 1)
   assert_that(length(n) == 1, n >= 0)
@@ -175,7 +188,7 @@ pred_grid <- function(model, data, n = 80, se = TRUE, level = 0.95) {
 }
 
 #' @export
-pred_grid.loess <- function(model, data, n = 80, se = TRUE, level = 0.95) {
+pred_grid.loess <- function(model, data, n = 80, se = FALSE, level = 0.95) {
   if (length(model$xnames) > 1) {
     stop("Only know how to make grid for one variable", call. = FALSE)
   }
@@ -202,7 +215,7 @@ pred_grid.loess <- function(model, data, n = 80, se = TRUE, level = 0.95) {
   }
 }
 
-pred_grid.lm <- function(model, data, n = 80, se = TRUE, level = 0.95) {
+pred_grid.lm <- function(model, data, n = 80, se = FALSE, level = 0.95) {
   x_var <- attr(terms(model), "term.labels")
   if (length(x_var) > 1) {
     stop("Only know how to make grid for one variable", call. = FALSE)
@@ -218,7 +231,7 @@ pred_grid.lm <- function(model, data, n = 80, se = TRUE, level = 0.95) {
   if (!se) {
     data.frame(
       pred_ = x_grid,
-      resp_ = as.vector(pred)
+      resp_ = as.vector(resp)
     )
   } else {
     data.frame(
