d509ccdc50d51f8798c6119e4ffc088fa9174cfc
hadley <h.wickham@gmail.com>
2014-05-01 09:22:39 -0500

    Layer_lines always sorts

--- R/layer_lines.R
+++ R/layer_lines.R
@@ -2,12 +2,8 @@
 #'
 #' @seealso \code{link{mark_path}}
 #' @export
-#' @param sort Should the data be sorted? If \code{TRUE} (the default), a
-#'   \code{\link{transform_sort}} will be used. By default, this will sort the
-#'   data on the x variable.
 #' @param vis Visualisation to modify.
-#' @param ... Named arguments are passed on to \code{\link{transform_sort}};
-#'   unnamed arguments are not used.
+#' @param ... Visual properties.
 #' @examples
 #' mtcars2 <- dplyr::mutate(mtcars, cyl = factor(cyl))
 #' mtcars2 %>% ggvis(~wt, ~mpg, stroke = ~cyl) %>% layer_lines()
@@ -15,13 +11,13 @@
 #' # Equivalent to
 #' mtcars2 %>% ggvis(~wt, ~mpg, stroke = ~cyl) %>%
 #'   group_by(cyl) %>% arrange(wt) %>% layer_paths()
-layer_lines <- function(vis, ..., sort = TRUE) {
+layer_lines <- function(vis, ...) {
 
   x_var <- vis$cur_props$x$value
 
   branch_f(vis, function(x) {
     x <- auto_group(x)
-    if (sort) x <- do_call(dplyr::arrange, quote(x), x_var)
+    x <- do_call(dplyr::arrange, quote(x), x_var)
     emit_paths(x, props(...))
   })
 }
