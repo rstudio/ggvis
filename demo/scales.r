d43f3c535405977418edd51e22490c595053bc82
hadley <h.wickham@gmail.com>
2014-05-01 09:36:13 -0500

    Update demo/scales

--- demo/scales.r
+++ demo/scales.r
@@ -2,13 +2,13 @@ library(ggvis)
 
 # Discrete colours for fill and a manual scale for opacity
 mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~factor(cyl), fillOpacity = ~hp) %>%
-  layer_point() %>%
+  layer_points() %>%
   set_dscale("opacity", "numeric", range = c(0.2, 1))
 
 # Unscaled values in the data
 mtc <- mtcars
 mtc$color <- c("red", "teal", "#cccccc", "tan")
-mtc %>% ggvis(x = ~wt, y = ~mpg, fill := ~color) %>% layer_point()
+mtc %>% ggvis(x = ~wt, y = ~mpg, fill := ~color) %>% layer_points()
 
 # Unscaled constant
-mtcars %>% ggvis(x = ~wt, y = ~mpg, fill := "red") %>% layer_point()
+mtcars %>% ggvis(x = ~wt, y = ~mpg, fill := "red") %>% layer_points()
