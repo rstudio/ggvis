f165a2deb1a6e7a2df742e3ed5c94acde0558da8
hadley <h.wickham@gmail.com>
2014-05-01 09:50:04 -0500

    Update demo/guides

--- demo/guides.r
+++ demo/guides.r
@@ -1,16 +1,16 @@
 library(ggvis)
 
 mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~cyl) %>%
-  layer_point()
+  layer_points()
 
 mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~cyl) %>%
-  layer_point() %>%
+  layer_points() %>%
   add_guide_axis("x", title = "Weight")
 
 mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~cyl) %>%
-  layer_point() %>%
+  layer_points() %>%
   add_guide_axis("x", title = "Weight", orient = "top")
 
 mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~cyl) %>%
-  layer_point() %>%
+  layer_points() %>%
   add_guide_legend(fill = "fill", title = "Cylinders")
