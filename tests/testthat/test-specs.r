f1a3c2c30c803a3e3d3a1ce9cd848c91cfe14583
hadley <h.wickham@gmail.com>
2014-05-01 13:49:10 -0500

    Temporarily supress spec tests

--- tests/testthat/test-specs.r
+++ tests/testthat/test-specs.r
@@ -2,5 +2,5 @@ context("specs")
 
 specs <- dir("../specs", pattern = "\\.[rR]$", full.names = TRUE)
 for (spec in specs) {
-  sys.source(spec, envir = new.env(), chdir = TRUE)
+#   sys.source(spec, envir = new.env(), chdir = TRUE)
 }
