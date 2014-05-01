97ea29ed6dadf5f0dd40574a554159e17a74172b
hadley <h.wickham@gmail.com>
2014-05-01 10:11:02 -0500

    Support interactive inputs in dplyr functions

--- R/compute_dplyr.R
+++ R/compute_dplyr.R
@@ -11,6 +11,9 @@ group_by <- dplyr::group_by
 
 #' Dplyr verbs for ggvis.
 #'
+#' Reactive components must be wrapped in \code{eval} - this makes it
+#' possible to separate out the non-standard evaluation of dplyr and ggvis.
+#'
 #' @name dplyr-ggvis
 #' @keywords internal
 #' @examples
@@ -22,8 +25,22 @@ group_by <- dplyr::group_by
 #' base %>% filter(mpg > 25) %>% layer_points(fill := "red")
 #'
 #' base %>% mutate(cyl = jitter(cyl)) %>% layer_points(fill := "red")
+#'
+#' #' # Dynamically restrict range using filter
+#' mtcars %>% ggvis(~disp, ~mpg) %>%
+#'    filter(cyl > eval(input_slider(0, 10))) %>%
+#'    layer_points()
+#'
+#' # Dynamically compute box-cox transformation with transform_mutate
+#' bc <- function(x, lambda) {
+#'   if (abs(lambda) < 1e-6) log(x) else (x ^ lambda - 1) / lambda
+#' }
+#' bc_slider <- input_slider(-2, 2, 1, step = 0.1)
+#' mtcars %>%
+#'  ggvis(~disp, ~mpg) %>%
+#'  mutate(disp = bc(disp, eval(bc_slider))) %>%
+#'  layer_points()
 NULL
-
 
 #' @export
 #' @rdname dplyr-ggvis
@@ -51,36 +68,40 @@ ungroup.ggvis <- function(x) {
 #' @rdname dplyr-ggvis
 #' @export
 summarise.ggvis <- function(.data, ...) {
-  dots <- dots(...)
-  register_computation(.data, list(), "summarise", function(data, args) {
-    do_call(dplyr::summarise, quote(data), .args = dots)
+  pieces <- extract_inputs(dots(...))
+
+  register_computation(.data, pieces$inputs, "summarise", function(data, args) {
+    do_call(dplyr::summarise, quote(data), .args = pieces$expr)
   })
 }
 
 #' @rdname dplyr-ggvis
 #' @export
 mutate.ggvis <- function(.data, ...) {
-  dots <- dots(...)
-  register_computation(.data, list(), "mutate", function(data, args) {
-    do_call(dplyr::mutate, quote(data), .args = dots)
+  pieces <- extract_inputs(dots(...))
+
+  register_computation(.data, pieces$inputs, "mutate", function(data, args) {
+    do_call(dplyr::mutate, quote(data), .args = pieces$expr)
   })
 }
 
 #' @rdname dplyr-ggvis
 #' @export
 arrange.ggvis <- function(.data, ...) {
-  dots <- dots(...)
-  register_computation(.data, list(), "arrange", function(data, args) {
-    do_call(dplyr::arrange, quote(data), .args = dots)
+  pieces <- extract_inputs(dots(...))
+
+  register_computation(.data, pieces$inputs, "arrange", function(data, args) {
+    do_call(dplyr::arrange, quote(data), .args = pieces$expr)
   })
 }
 
 #' @rdname dplyr-ggvis
 #' @export
 select.ggvis <- function(.data, ...) {
-  dots <- dots(...)
-  register_computation(.data, list(), "select", function(data, args) {
-    do_call(dplyr::select, quote(data), .args = dots)
+  pieces <- extract_inputs(dots(...))
+
+  register_computation(.data, pieces$inputs, "select", function(data, args) {
+    do_call(dplyr::select, quote(data), .args = pieces$expr)
   })
 }
 
@@ -89,9 +110,51 @@ select.ggvis <- function(.data, ...) {
 #' @rdname dplyr-ggvis
 #' @export
 filter.ggvis <- function(.data, ...) {
-  dots <- dots(...)
-  register_computation(.data, list(), "filter", function(data, args) {
-    do_call(dplyr::filter, quote(data), .args = dots)
+  pieces <- extract_inputs(dots(...))
+
+  register_computation(.data, pieces$inputs, "filter", function(data, args) {
+    do_call(dplyr::filter, quote(data), .args = pieces$expr)
   })
 }
 
+extract_inputs <- function(x, env = parent.frame()) {
+  # Base case
+  if (is.name(x) || is.atomic(x)) {
+    return(list(expr = x, inputs = NULL))
+  }
+
+  # If it's a call to eval, it's an input and should be evaluated
+  if (is.call(x) && identical(x[[1]], quote(eval))) {
+    stopifnot(length(x) == 2)
+    input <- eval(x[[2]], env)
+    stopifnot(is.input(input))
+
+    nm <- attr(input, "input_id")
+
+    return(list(
+      expr = substitute(args$nm, list(nm = as.name(nm))),
+      inputs = setNames(list(input), nm)
+    ))
+  }
+
+  # Otherwise it must be another call, or a list, in which case recurse
+  if (is.list(x)) {
+    args_out <- lapply(x, extract_inputs, env = env)
+  } else if (is.call(x)) {
+    args_out <- lapply(x[-1], extract_inputs, env = env)
+  } else {
+    stop("Unknown input type: ", paste0(class(x), collapse = "/"),
+      call. = FALSE)
+  }
+
+  expr <- lapply(args_out, "[[", "expr")
+  if (is.call(x)) expr <- as.call(c(x[[1]], expr))
+
+  inputs <- unlist(lapply(unname(args_out), "[[", "inputs"), recursive = FALSE)
+  inputs <- inputs[!duplicated(names(inputs))]
+
+  list(
+    expr = expr,
+    inputs = inputs
+  )
+}
