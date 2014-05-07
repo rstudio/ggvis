ee6f6fd320d0c688e58db1f5fb33fa599735f5a4
hadley <h.wickham@gmail.com>
2014-05-07 09:38:49 -0500

    Use handler in brush-summary

--- demo/apps/brush-summary/server.r
+++ demo/apps/brush-summary/server.r
@@ -3,36 +3,34 @@ library(ggvis)
 data(diamonds, package = "ggplot2")
 diamonds <- diamonds[sample(1:nrow(diamonds), 1000), ]
 
+brush_summary <- function(items) {
+  if (length(items) == 0) {
+    return(data.frame(wt = numeric(0), mpg = numeric(0)))
+  }
+
+  data.frame(
+    wt  = vapply(items, `[[`, "wt",  FUN.VALUE = numeric(1)),
+    mpg = vapply(items, `[[`, "mpg", FUN.VALUE = numeric(1))
+  )
+}
+
 shinyServer(function(input, output, session) {
+  under_brush <- function(value, ...) {
+    df <- brush_summary(value$items)
+
+    output$brush_data <- renderPrint({
+      cat("Number of points selected: ", nrow(df), "\n\n")
+      print(summary(df))
+    })
+  }
 
   gv <- reactive({
     mtcars %>% ggvis(~wt, ~mpg, fill.brush := "blue") %>%
       layer_points() %>%
-      layer_brush() %>%
-      set_options(brush_delay = 100)
+      handle_brush(under_brush)
   })
 
   # Set up observers for the spec and the data
   observe_ggvis(gv, "plot1", session)
-
-  # Return a data frame of the brushed points
-  brushed_items <- reactive({
-    items <- input$ggvis_plot1_brush$items
-
-    if (is.null(items) || length(items) == 0) {
-      return(data.frame(wt = numeric(0), mpg = numeric(0)))
-    }
-
-    data.frame(
-      wt  = vapply(items, `[[`, "wt",  FUN.VALUE = numeric(1)),
-      mpg = vapply(items, `[[`, "mpg", FUN.VALUE = numeric(1))
-    )
-  })
-
-  # Print the object that was sent over
-  output$brush_data <- renderPrint({
-    cat("Number of points selected: ", nrow(brushed_items()), "\n\n")
-    print(summary(brushed_items()))
-  })
 
 })
