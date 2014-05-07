239547914dc5b4c4465527676a2086fedd7c8555
hadley <h.wickham@gmail.com>
2014-05-07 10:27:26 -0500

    Update brush-linked

--- demo/apps/brush-linked/server.r
+++ demo/apps/brush-linked/server.r
@@ -7,19 +7,17 @@ shinyServer(function(input, output, session) {
 
   lb <- linked_brush(keys = 1:nrow(diamonds))
 
-  gv1 <- reactive({
-    diamonds %>% ggvis(~carat, ~price) %>%
-      layer_points(fill := lb$fill(), fillOpacity := 0.8,
-        fill.brush := "red") %>%
-      lb$input() %>%
-      set_options(width = 300, height = 300)
-  })
+  gv1 <- diamonds %>%
+    ggvis(~carat, ~price) %>%
+    layer_points(fill := lb$fill(), fillOpacity := 0.8,
+      fill.brush := "red") %>%
+    lb$input() %>%
+    set_options(width = 300, height = 300)
 
-  gv2 <- reactive({
-    diamonds %>% ggvis(~table, ~depth) %>%
-      layer_points(fill := lb$fill(), fillOpacity := 0.8) %>%
-      set_options(width = 300, height = 300)
-  })
+  gv2 <- diamonds %>%
+    ggvis(~table, ~depth) %>%
+    layer_points(fill := lb$fill(), fillOpacity := 0.8) %>%
+    set_options(width = 300, height = 300)
 
   # Set up observers for the spec and the data
   observe_ggvis(gv1, "plot1", session)
