81030d3a8d16c4b76fd45c8e36a468da87232812
hadley <h.wickham@gmail.com>
2014-05-07 10:26:32 -0500

    Update brush-summary

--- demo/apps/brush-summary/server.r
+++ demo/apps/brush-summary/server.r
@@ -4,20 +4,17 @@ data(diamonds, package = "ggplot2")
 diamonds <- diamonds[sample(1:nrow(diamonds), 1000), ]
 
 shinyServer(function(input, output, session) {
-  under_brush <- function(items, ...) {
-    output$brush_data <- renderPrint({
-      cat("Number of points selected: ", nrow(items), "\n\n")
-      print(summary(items))
+  # Create plot and set up observers
+  gv <- mtcars %>%
+    ggvis(~wt, ~mpg) %>%
+    layer_points(fill.brush := "blue") %>%
+    handle_brush(function(items, ...) {
+      output$brush_data <- renderPrint({
+        cat("Number of points selected: ", nrow(items), "\n\n")
+        print(summary(items))
+      })
     })
-  }
-
-  gv <- reactive({
-    mtcars %>% ggvis(~wt, ~mpg, fill.brush := "blue") %>%
-      layer_points() %>%
-      handle_brush(under_brush)
-  })
 
   # Set up observers for the spec and the data
   observe_ggvis(gv, "plot1", session)
-
 })
