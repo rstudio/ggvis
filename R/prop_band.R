ba4087d78ef02f08d2048c341543ad9bd611ebe3
Winston Chang <winston@stdout.org>
2014-06-09 22:21:31 -0500

    Make sure band is scaled

--- R/prop_band.R
+++ R/prop_band.R
@@ -10,17 +10,16 @@
 #' @examples
 #' df <- data.frame(label = c("a", "b", "c"), n = c(10, 9, 4))
 #'
-#' base <- df %>% ggvis(~label, y2 = 0, y := ~n) %>%
+#' base <- df %>% ggvis(~label, y2 = 0, y = ~n) %>%
 #'   scale_nominal("x", padding = 0, points = FALSE)
-#' base %>% layer_rects(width := band())
-#' base %>% layer_rects(width := band(offset = -1))
-#' base %>% layer_rects(width := band(mult = 0.9))
+#' base %>% layer_rects(width = band())
+#' base %>% layer_rects(width = band(offset = -1))
+#' base %>% layer_rects(width = band(mult = 0.9))
 #'
 #' # A nominal scale with padding is more symmetrical than band with a mult
-#' base %>% layer_rects(width := band(mult = 0.75))
-#' base %>% layer_rects(width := band()) %>%
+#' base %>% layer_rects(width = band(mult = 0.75))
+#' base %>% layer_rects(width = band()) %>%
 #'   scale_nominal("x", padding = 0.25, points = FALSE)
-#'
 band <- function(offset = NULL, mult = NULL) {
   structure(
     list(offset = offset, mult = mult),
@@ -34,11 +33,12 @@ new_prop.band <- function(x, property, scale, offset, mult, env, event,
   if (!(property %in% c("width", "height"))) {
     stop("band() can only be used for width and height properties.")
   }
+  if (identical(scale, FALSE)) stop("band() must be scaled.")
 
   structure(
     list(
       property = property,
-      scale = decide_scale(scale %||% TRUE, property),
+      scale = decide_scale(scale, property),
       offset = offset,
       mult = mult,
       event = event,
