f44aeb4632ce4f64049f8b2fc4e7ee01d7893a85
Winston Chang <winston@stdout.org>
2014-06-17 15:17:12 -0500

    Add magrittr pipe import

--- R/pipe.R
+++ R/pipe.R
@@ -3,6 +3,7 @@
 #' Like dplyr, ggvis also uses the pipe function, \code{\%>\%} to turn
 #' function composition into a series of imperative statements.
 #'
+#' @importFrom magrittr %>%
 #' @export
 #' @param lhs,rhs A visualisation and a function to apply to it
 #' @rdname pipe
