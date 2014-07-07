6d831ee553d7e0b1a659a25b3b0cfbfcca0c36f6
Winston Chang <winston@stdout.org>
2014-07-07 15:34:03 -0500

    Properly re-export magrittr::%>%

--- R/pipe.R
+++ R/pipe.R
@@ -4,12 +4,13 @@
 #' function composition into a series of imperative statements.
 #'
 #' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
 #' @export
 #' @param lhs,rhs A visualisation and a function to apply to it
-#' @rdname pipe
 #' @examples
 #' # Instead of
 #' layer_points(ggvis(mtcars, ~mpg, ~wt))
 #' # you can write
 #' mtcars %>% ggvis(~mpg, ~wt) %>% layer_points()
-`%>%` <- magrittr::`%>%`
+NULL
