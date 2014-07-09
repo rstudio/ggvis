bd0bdf6ba300ab7ecd0f3e8cf68b2a0282267bbe
Winston Chang <winston@stdout.org>
2014-07-08 23:14:19 -0500

    Add tests for compute_bin

---
+++ tests/testthat/test-compute-bin.r
@@ -0,0 +1,22 @@
+context("compute_bin")
+
+test_that("count_vector correctly handles factors", {
+  fac <- factor(c("A", "A", "C"), levels = c("A", "C"))
+  expect_identical(
+    count_vector(fac),
+    data.frame(
+      count_ = c(2, 1),
+      x_ = factor(c("A", "C"), levels = c("A", "C"))
+    )
+  )
+
+  # Levels that aren't represented are dropped from the count
+  fac <- factor(c("A", "A", "C"), levels = c("A", "B", "C"))
+  expect_identical(
+    count_vector(fac),
+    data.frame(
+      count_ = c(2, 1),
+      x_ = factor(c("A", "C"), levels = c("A", "B", "C"))
+    )
+  )
+})
