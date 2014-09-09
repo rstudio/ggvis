289a51c7f5ac1e5005af3e92d5d2e5674a7bdea4
Winston Chang <winston@stdout.org>
2014-09-09 15:56:37 -0500

    Add spec tests for boxplot without outliers

--- tests/specs/boxplot.r
+++ tests/specs/boxplot.r
@@ -11,3 +11,10 @@ mtc %>%
   ggvis(~cyl, ~mpg) %>%
   layer_boxplots() %>%
   save_spec("boxplot/boxplot-categorical.json")
+
+
+dat <- data.frame(x = c('a','a','a','b','b','b'), y = c(1:3, 2:4))
+dat %>%
+  ggvis(x = ~x, y = ~y) %>%
+  layer_boxplots() %>%
+  save_spec("boxplot/boxplot-no-outliers.json")
