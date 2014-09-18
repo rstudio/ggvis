62306ee4587e8af1171786c7f05b060076886166
Winston Chang <winston@stdout.org>
2014-09-17 21:34:28 -0500

    Fix up tests for change in all.equal behavior
    R-devel (will be R 3.2.0) has a change in behavior of all.equal for environments.
    This change fixes the tests for this new behavior.

--- tests/testthat/test-ggvis.R
+++ tests/testthat/test-ggvis.R
@@ -1,6 +1,22 @@
 context("ggvis")
+# Recursively crawl a list and replace any environments with a special
+# environment. This is a workaround for a change in behavior in R 3.2.0
+# for all.equal when given two environments.
+blank_envs <- function(x) {
+  replacement_env <- new.env(parent = emptyenv())
+
+  # Use `x[]<-` to preserve any attributes on x
+  x[] <- lapply(x, function(val) {
+    if (is.environment(val)) replacement_env
+    else if (is.list(val)) blank_envs(val)
+    else val
+  })
+
+  x
+}
 
 test_that("plot the same regardless of order data/props added", {
+
   df <- data.frame(x = 1:10, y = 10:1)
   plots <- list(
     df %>% ggvis(~x, ~y) %>% layer_points(),
@@ -9,12 +25,17 @@ test_that("plot the same regardless of order data/props added", {
     ggvis() %>% add_data(df) %>% add_props(~x, ~y) %>% layer_points(),
     ggvis() %>% add_data(df) %>% add_props(~x) %>% layer_points(y = ~y)
   )
+  plots <- blank_envs(plots)
+
   marks <- lapply(plots, function(x) x$marks[[1]])
   props <- lapply(marks, "[[", "props")
   data  <- lapply(marks, function(x) x$data())
 
   for (i in seq_along(marks)) {
-    expect_equal(props[[i]][c("x.update", "y.update")], props(~x, ~y))
+    expect_equal(
+      props[[i]][c("x.update", "y.update")],
+      blank_envs(props(~x, ~y))
+    )
     expect_equal(data[[i]], df)
   }
 })
