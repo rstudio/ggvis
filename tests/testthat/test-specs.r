7692a4edfa6e932095dfbb49e20f8489eef8fd7d
hadley <h.wickham@gmail.com>
2014-09-29 15:13:08 -0500

    Suppress spec messages

--- tests/testthat/test-specs.r
+++ tests/testthat/test-specs.r
@@ -2,5 +2,7 @@ context("specs")
 
 specs <- dir("../specs", pattern = "\\.[rR]$", full.names = TRUE)
 for (spec in specs) {
+  suppressMessages(
    sys.source(spec, envir = new.env(), chdir = TRUE)
+  )
 }
