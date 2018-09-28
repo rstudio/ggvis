bca469ad353ab28b61b25fc8b6a32172cc3d0a9c
Winston Chang <winston@stdout.org>
2018-09-28 13:31:14 -0500

    Remove broken link

--- R/data_cocaine.R
+++ R/data_cocaine.R
@@ -18,6 +18,4 @@
 #' "unvalidated DEA data and to claim authorship and responsibility for any
 #' inferences and/or conclusions you may draw from this information."
 #' @format Data frame with 3380 observations of 5 variables.
-#' @source
-#' \url{http://www.justice.gov/dea/resource-center/stride-data.shtml}
 "cocaine"
